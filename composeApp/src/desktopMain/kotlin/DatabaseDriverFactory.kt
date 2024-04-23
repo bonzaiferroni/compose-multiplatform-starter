6e17a9e7eb44a29462727403ba600b79e9895e6a
Luke Bollwerk <lukas8@gmail.com>
2024-04-23 14:22:40 -0600

    fix disappearing data

--- composeApp/src/desktopMain/kotlin/DatabaseDriverFactory.kt
+++ composeApp/src/desktopMain/kotlin/DatabaseDriverFactory.kt
@@ -1,10 +1,15 @@
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.example.ExampleDb
+import java.io.File
 
 actual class DatabaseDriverFactory {
     actual fun create(): SqlDriver {
-        val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+        // "jdbc:sqlite:example.db"
+        // val dbFile = File("example.db")
+        // val driver: SqlDriver = JdbcSqliteDriver(dbFile.absolutePath)
+        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:../example.db")
+        // val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
         ExampleDb.Schema.create(driver)
         return driver
     }
