277ced9e44f72b1342ae39bd0ef0c4056ba4d602
Luke Bollwerk <lukas8@gmail.com>
2024-04-25 08:46:51 -0600

    move chopui to separate module

--- composeApp/src/commonMain/kotlin/ui/ExtraScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/ExtraScreen.kt
@@ -2,7 +2,6 @@ package ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import chopui.Scaffold_Old
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
